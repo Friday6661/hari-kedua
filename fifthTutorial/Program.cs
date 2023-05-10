50093af6dcdd04b93176e22d89c97e6361f3aea6
Friday6661 <jarifr26@gmail.com>
2023-05-10 16:16:09 +0700

    commit hari ke-3 part1

---
+++ fifthTutorial/Program.cs
@@ -0,0 +1,24 @@
+﻿// Hari ke-4
+
+using inheritance;
+
+static class Program {
+    static void Main() {
+        Cars cars = new Cars("001", 6661, "dark");
+        sportCars sportcars = new sportCars("C", 2002, "dark", "jarvis", "Sport");
+        jeapCars jeapcars = new jeapCars();
+
+
+        Console.WriteLine(cars.name);
+        //Console.WriteLine(sportcars._machineCode);
+        Console.WriteLine(sportcars.name);
+        Console.WriteLine(cars.yearProduct);
+        Console.WriteLine(sportcars.yearProduct);
+        Console.WriteLine(sportcars.variance);
+
+        sportcars.breaking();
+        sportcars.sportCarsMethode();
+        jeapcars.jeapCarsMethode();
+
+    }
+}
