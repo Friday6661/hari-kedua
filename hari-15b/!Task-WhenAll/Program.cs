535ff9deaaf778725fdc7a963110bfa179033cca
Friday6661 <jarifr26@gmail.com>
2023-06-14 13:35:28 +0700

    FileStream dan StreamWriter

---
+++ hari-15b/!Task-WhenAll/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine($"Main Thread started");
+        List<CreditCard> creditCards = creditCards.GenerateCreditCard(10);
+        Console.WriteLine($"Credit Card Generated : {creditCards.Count}");
+
+        ProcessCreditCards(creditCards);
+
+        Console.WriteLine($"Main Thread Completed");
+        Console.ReadKey();
+    }
+
+    public static async void ProcessCreditCards(List<CreditCard> creditCards)
+    {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        foreach (var creditCard in creditCards)
+        {
+            var response = await ProcessCreditCards(creditCard);
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine($"Processing of {creditCards.Count} Credit Cards Done in {stopwatch.ElapsedMilliseconds/ 1000} Seconds");
+    }
+
+    public static async Task<string> ProcessCard(CreditCard creditCard)
+    {
+        await Task.Delay(1000);
+        string message = $"Credit Card Number: {creditCard.CardNumber} Name: {creditCard.Name} Processed";
+        Console.WriteLine($"Credit Card Number: {creditCard.CardNumber} Processed");
+        return message;
+    }
+
+    public class CreditCard
+    {
+        public string CardNumber { get; set; }
+        public string Name { get; set; }
+
+        public static List<CreditCard> GenerateCreditCards(int number)
+        {
+            List<CreditCard> cards = new List<CreditCard>();
+            for (int i = 0; i < number; i++)
+            {
+                CreditCard card = new CreditCard()
+                {
+                    CardNumber = "1000000" + i,
+                    Name = "Credit Card-" + i
+                }
+            }
+        }
+    }
+}
---
+++ hari-16/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = @"MyFile.txt";
+        int dataSizeInMB = 100; // Ukuran data yang akan ditulis dalam MB
+
+        Console.WriteLine("Mulai pengujian FileStream...");
+        TestFileStream(filePath, dataSizeInMB);
+
+        Console.WriteLine("\nMulai pengujian StreamWriter dengan MemoryStream...");
+        TestStreamWriter(filePath, dataSizeInMB);
+
+        Console.ReadKey();
+    }
+
+    static void TestFileStream(string filePath, int dataSizeInMB)
+    {
+        byte[] data = GenerateData(dataSizeInMB);
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        using (FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+        {
+            fileStream.Write(data, 0, data.Length);
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine("Pengujian FileStream selesai. Waktu yang dibutuhkan: " + stopwatch.Elapsed);
+    }
+
+    static void TestStreamWriter(string filePath, int dataSizeInMB)
+    {
+        byte[] data = GenerateData(dataSizeInMB);
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        using (MemoryStream memoryStream = new MemoryStream())
+        using (StreamWriter writer = new StreamWriter(memoryStream))
+        {
+            writer.Write(data);
+            writer.Flush();
+
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                memoryStream.WriteTo(fileStream);
+            }
+        }
+
+        stopwatch.Stop();
+        Console.WriteLine("Pengujian StreamWriter dengan MemoryStream selesai. Waktu yang dibutuhkan: " + stopwatch.Elapsed);
+    }
+
+    static byte[] GenerateData(int sizeInMB)
+    {
+        int dataSize = sizeInMB * 1024 * 1024;
+        byte[] data = new byte[dataSize];
+
+        // Mengisi data dengan byte acak
+        new Random().NextBytes(data);
+
+        return data;
+    }
+}
